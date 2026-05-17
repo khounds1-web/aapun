c146a3ddb0cde1887491e8a4c63185737214acef
Sumana Khound <khoundsumana@gmail.com>
2026-05-16 20:31:27 -0700

    Fix layout height causing black screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,9 @@ export default async function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">
+      <body className="h-full">
         <ClerkProvider>
-          <div className="flex min-h-0 flex-1 flex-col">{children}</div>
+          {children}
         </ClerkProvider>
       </body>
     </html>
